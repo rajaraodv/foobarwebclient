1de0a63ac40f389885dde6ccda3619e0637838ad
Raja Rao DV <rajar@vmware.com>
2012-11-26 10:38:48 -0800

    remove photoCtrl.js

--- app/scripts/photoCtrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-function PhotoCtrl($scope, $http) {
-  $http.get('scripts/posts.json').success(function(data) {
-    $scope.photoPosts = data;
-  });
-}
-
-PhotoCtrl.$inject = ['$scope', '$http'];
