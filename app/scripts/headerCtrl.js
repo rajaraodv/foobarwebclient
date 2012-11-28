39db874686ec7f58dd90a2c64b32a2783e9927a7
Raja Rao DV <rajar@vmware.com>
2012-11-28 15:22:54 -0800

    Add Logout support

--- app/scripts/headerCtrl.js
+++ app/scripts/headerCtrl.js
@@ -1,12 +1,17 @@
 'use strict';
 
-function HeaderCtrl($scope, LoginService, $rootScope) {
+function HeaderCtrl($scope, LoginService, $rootScope, $location) {
 	//Note: setting value to rootScope makes it available for EVERY controller (via $rootscope)
 	$rootScope.appUser = LoginService.get();
 	$scope.$watch('appUser', function() {
 		$scope.loginText = $scope.appUser.username ? 'Logged in as ' + $scope.appUser.username : 'Log In';
 		$rootScope.loggedIn = $scope.appUser.username ? true : false;
 	}, true);
+
+	$scope.logout = function () {
+		$rootScope.appUser = '';
+		$location.path('/');
+	};
 }
 
-HeaderCtrl.$inject = ['$scope', 'LoginService', '$rootScope'];
+HeaderCtrl.$inject = ['$scope', 'LoginService', '$rootScope', '$location'];
